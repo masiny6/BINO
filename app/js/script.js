749d1a9078b13b8dbd765df08eed7fd616db0301
masiny <suvorkin_2001@mail.ru>
2020-09-29 11:02:37 +0300

    Tablet верстка v0.90

--- app/js/script.js
+++ app/js/script.js
@@ -10,3 +10,35 @@ $(".form__input-subject").on("click", function() {
 $(".form__textarea").on("click", function() {
     $(".form__label-message").addClass("label-textarea-js");
 })
+
+if (window.pageYOffset > 1010) {
+    $(".arrow").addClass("arrow-js");
+} else {
+    $(".arrow").removeClass("arrow-js");
+}
+$(window).on("scroll", function() {
+    if (window.pageYOffset > 1010) {
+        $(".arrow").addClass("arrow-js");
+    } else {
+        $(".arrow").removeClass("arrow-js");
+    }
+})
+$(".arrow-js").on("click", function() {
+    $('html, body').animate({scrollTop: 0},600);
+    return false;
+})
+
+// Бургер меню
+$(".burger-menu").on("click", function(){
+    if ($(".burger-menu").prop("checked")) {
+        $(".menu").css("transform", "translateX(0px)");
+        $(".menu__item").css("transform", "scale(1)");
+        $(".menu__item").css("transform", "rotate(0deg)");
+        $(".menu__item").css("margin-left", "50px");
+    } else {
+        $(".menu").css("transform", "translateX(-5000px)");
+        $(".menu__item").css("transform", "scale(5)");
+        $(".menu__item").css("transform", "rotate(360deg)");
+        $(".menu__item").css("margin-left", "200px");
+    }
+});
